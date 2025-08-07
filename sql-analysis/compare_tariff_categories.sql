aa151bb9f04f2e9a67a226746eb0101340bbadd7
SorinCopaci <sorin.copaci@gmail.com>
2025-08-07 15:05:05 +0100

    Update compare_tariff_categories.sql

--- sql-analysis/compare_tariff_categories.sql
+++ sql-analysis/compare_tariff_categories.sql
@@ -1,1 +1,8 @@
+SELECT
+  tariff_category,
+  ROUND(AVG(price_pence) / 100.0, 2) AS avg_price_gbp,
+  COUNT(*) AS product_count
+FROM tariff
+GROUP BY tariff_category
+ORDER BY avg_price_gbp DESC;
 
